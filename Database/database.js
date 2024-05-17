f4a77c279624488f613336c3e8ca04f33b3586be
arunkumargurram01 <144266255+arunkumargurram01@users.noreply.github.com>
2024-05-17 07:48:18 +0530

    Update database.js

--- Database/database.js
+++ Database/database.js
@@ -8,7 +8,9 @@ const pool = createPool({
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
+    port: process.env.DB_PORT,
     connectionLimit: process.env.DB_CONNECTION_LIMIT || 10,
+    connectTimeout: 10000
 });
 
 // Promisify the query function for async/await
